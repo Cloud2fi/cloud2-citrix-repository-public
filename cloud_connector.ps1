b7f7ebe537669c2b45e00aa77f7f4ab71cbc4356
c2valsta <50828792+c2valsta@users.noreply.github.com>
2022-12-15 14:15:15 +0200

    Update cloud_connector.ps1

--- cloud_connector.ps1
+++ cloud_connector.ps1
@@ -21,10 +21,10 @@ if (Test-Path -Path "c:\temp") {
 Invoke-WebRequest "https://downloads.cloud.com/cloudconnector/connector/cwcconnector.exe" -OutFile "C:\temp\cwcconnector.exe"
 
 # Create JSON file used by Cloud Connector installer
-New-Object -TypeName PSCustomObject -Property @{customerName=$customerName;clientId=$clientId;clientSecret=$clientSecret;resourceLocationId=$resourceLocationId;acceptTermsOfService="true"} | ConvertTo-Json | Out-File 'C:\temp\cwcconnector.json' 
+New-Object -TypeName PSCustomObject -Property @{customerName=$customerName;clientId=$clientId;clientSecret=$clientSecret;resourceLocationId=$resourceLocationId;acceptTermsOfService="true"} | ConvertTo-Json | Out-File "C:\temp\cwcconnector.json" 
 
 # Install Citrix Cloud Connector using JSON parameters file
-Start-Process C:\temp\cwcconnector.exe /q /ParametersFilePath:c:\temp\cwcconnector.json
+C:\temp\cwcconnector.exe /q /ParametersFilePath:c:\temp\cwcconnector.json
 
 # Cleanup created files
 Remove-Item -Path c:\temp\cwcconnector.json
